f59d2789f1f631d776b69841610830e87e9b9dcd
dadtmt <thomas.culdaut@gmail.com>
2020-12-08 18:26:33 +0100

    switch to routes

--- src/routes/wilder.ts
+++ src/routes/wilder.ts
@@ -1,9 +1,11 @@
-import { Request, Response } from 'express';
-import { validationResult } from 'express-validator';
+import { Request, Response, Router } from 'express';
+import { body, validationResult } from 'express-validator';
+import asyncHandler from 'express-async-handler';
+
 import InputError from '../errors/InputError';
 import WilderModel from '../models/Wilder';
 
-export default {
+const controller = {
   create: async (req: Request, res: Response): Promise<void> => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -29,3 +31,28 @@ export default {
     res.json({ success: true, result });
   },
 };
+
+const router = Router();
+
+router
+  .route('/api/wilders')
+  .post(
+    [
+      body('name')
+        .isLength({ min: 3 })
+        .withMessage('name must be at least 3 characters long'),
+      body('city').isString().withMessage('city must be a string'),
+      body('skills.*.title')
+        .isLength({ min: 2 })
+        .withMessage('skill title must be at least 2 characters long'),
+      body('skills.*.votes')
+        .isInt({ min: 0 })
+        .withMessage('skill votes must be an integer greater or equal to 0'),
+    ],
+    asyncHandler(controller.create)
+  )
+  .get(asyncHandler(controller.read))
+  .put(asyncHandler(controller.update))
+  .delete(asyncHandler(controller.delete));
+
+export default router;
