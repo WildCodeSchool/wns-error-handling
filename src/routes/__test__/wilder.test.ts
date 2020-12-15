39872a2d8f29e60abb1b97ea235bd3ac3f9b4888
dadtmt <thomas.culdaut@gmail.com>
2020-12-15 12:01:01 +0100

    test response length

--- src/routes/__test__/wilder.test.ts
+++ src/routes/__test__/wilder.test.ts
@@ -2,11 +2,12 @@ import request from 'supertest';
 import app from '../../app';
 
 it('returns a 201 on successful wilder creation', async () => {
-  return request(app)
+  const response = await request(app)
     .post('/api/wilders')
     .send({
       name: 'test name',
       city: 'New York',
     })
     .expect(201);
+  expect(response.body).toHaveLength(1);
 });
