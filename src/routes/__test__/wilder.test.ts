bff489e37cd61909f10b090e49c62d2ced036a1f
dadtmt <thomas.culdaut@gmail.com>
2020-12-15 12:09:14 +0100

    returns a 400 on a duplicate name

--- src/routes/__test__/wilder.test.ts
+++ src/routes/__test__/wilder.test.ts
@@ -1,13 +1,32 @@
 import request from 'supertest';
 import app from '../../app';
 
-it('returns a 201 on successful wilder creation', async () => {
-  const response = await request(app)
-    .post('/api/wilders')
-    .send({
-      name: 'test name',
+describe('Create a wilder', () => {
+  it('returns a 201 and the created wilder', async () => {
+    const name = 'test name';
+
+    const response = await request(app)
+      .post('/api/wilders')
+      .send({
+        name,
+        city: 'New York',
+      })
+      .expect(201);
+    expect(response.body.result.name).toEqual(name);
+  });
+  it('returns a 400 on a duplicate name', async () => {
+    const name = 'test name';
+
+    await request(app).post('/api/wilders').send({
+      name,
       city: 'New York',
-    })
-    .expect(201);
-  expect(response.body).toHaveLength(1);
+    });
+    await request(app)
+      .post('/api/wilders')
+      .send({
+        name,
+        city: 'Los Angeles',
+      })
+      .expect(400);
+  });
 });
